744c50d7ea772cdd9d635393befc900ee3c0b725
DziedzicFilip <dziedzicfilip00@gmail.com>
2025-04-07 11:50:13 +0200

    Wyswietlanie karrty filmu

--- MobileApp/SeenIT/SeenIT/ViewModels/AboutViewModel.cs
+++ MobileApp/SeenIT/SeenIT/ViewModels/AboutViewModel.cs
@@ -11,6 +11,7 @@ namespace SeenIT.ViewModels
         public Command OpenProfilePageCommand { get; }
         public ICommand OpenWebCommand { get; }
         public ICommand OpenAboutPage { get; }
+        public Command OpenObjectCommand { get; }
 
         public AboutViewModel()
         {
@@ -19,6 +20,7 @@ namespace SeenIT.ViewModels
             OpenAboutPage = new Command(async () => await Shell.Current.GoToAsync($"//{nameof(AboutPage)}"));
             OpenListPageCommand = new Command(OnOpenListPage);
             OpenProfilePageCommand = new Command(OnOpenProfilePage);
+            OpenObjectCommand = new Command(OpenObjectPage);
         }
         private async void OnOpenListPage()
         {
@@ -28,6 +30,10 @@ namespace SeenIT.ViewModels
         private async void OnOpenProfilePage()
         {
             await Shell.Current.GoToAsync($"//{nameof(ProfilePage)}");
+        }
+        private async void OpenObjectPage()
+        {
+            await Shell.Current.GoToAsync($"//{nameof(ObjectPage)}");
         }
 
     }
