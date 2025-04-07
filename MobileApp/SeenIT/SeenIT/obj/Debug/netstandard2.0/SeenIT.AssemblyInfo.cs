9eb9744bb51a02e990ccb337879a8883f8e992ff
DziedzicFilip <dziedzicfilip00@gmail.com>
2025-04-07 08:45:32 +0200

    strona logowania

--- MobileApp/SeenIT/SeenIT/obj/Debug/netstandard2.0/SeenIT.AssemblyInfo.cs
+++ MobileApp/SeenIT/SeenIT/obj/Debug/netstandard2.0/SeenIT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeenIT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+676d713432ec7497e773732ca460c982190cb419")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+327575695fa057c40af684967f2dcd9ff1ee450b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeenIT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeenIT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
